d0bfc437c3bec7252cfc7913b608240eaa2d2bd3
hahaschool <hahaschool.wing@gmail.com>
2022-02-20 21:05:41 +0800

    Add collator address of Moonbeam

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,24 +7,26 @@
       <h3 class="title">Moonriver Network</h3>
       <el-col :span="24">
         <p style="word-break: break-word;">
-          Collator address: 0x8262f26CAD7855061a9bE91f8A7e1896225FD066
+          Collator name: 255 DAO | 🌙🌊 <br/>
+          Collator address: <a href="https://moonbeam.subscan.io/validator/0x8262f26CAD7855061a9bE91f8A7e1896225FD066">0x8262f26CAD7855061a9bE91f8A7e1896225FD066</a>
+        </p>
+      </el-col>
+    </el-row>
+
+    <el-row class="row-bg nes-container with-title is-rounded" justify="center">
+      <h3 class="title">Moonbeam Network</h3>
+      <el-col :span="24">
+        <p style="word-break: break-word;">
+          Collator name: 255 DAO | 🌔✨ <br/>
+          Collator address: <a href="https://moonbeam.subscan.io/validator/0xE596665a52D9Bb37865b3a9264fFE038Be08F28d">0xE596665a52D9Bb37865b3a9264fFE038Be08F28d</a>
         </p>
 
       </el-col>
     </el-row>
 
-    <el-row class="row-bg" justify="center">
-    <div class="nes-container with-title is-rounded">
-      <h3 class="title">Moonbeam Network</h3>
-      <p>Coming soon ...</p>
-    </div>
-    </el-row>
-
-    <el-row  class="row-bg" justify="center">
-    <div class="nes-container with-title is-rounded">
+    <el-row  class="row-bg nes-container with-title is-rounded" justify="center">
       <h3 class="title">Calamari Network</h3>
       <p>Coming soon ...</p>
-    </div>
     </el-row>
 
     <el-row type="flex" class="row-bg" justify="center">
